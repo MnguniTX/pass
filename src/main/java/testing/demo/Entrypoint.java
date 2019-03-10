e779281a0598cc472030abc493ef43cf15fd55b3
MnguniTX <xolanithabo@gmail.com>
2019-03-10 22:43:28 +0200

    new

--- src/main/java/testing/demo/Entrypoint.java
+++ src/main/java/testing/demo/Entrypoint.java
@@ -1,4 +1,4 @@
-package testing.demo.apicontroller;
+package testing.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
